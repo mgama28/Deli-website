2c7d7729bf8b6b6f4a09b58128eedbe44ca0560a
Maria Gama <mgama288@gmail.com>
2025-09-04 15:02:23 -0500

    Cleaned up the public folder

--- public/js/miniNav.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const miniNav = document.getElementById("miniNav");
-    miniNav.className = "mini-Nav";
-    miniNav.innerHTML = 
-    <ul>
-        <li></li>
-    </ul>
